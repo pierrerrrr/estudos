d0d43ed8cadd3893aac02b6526aa5c6926ef2b4e
Iago Pierre <pierrestorniolo@gmail.com>
2025-04-14 00:00:10 -0300

    up: just a simple conditional test

---
+++ classroom/rocketseat/logica/teste-condicional.py
@@ -0,0 +1,11 @@
+# DETRAN
+
+age = int(input("Qual a sua idade? "))
+
+if age >= 18: 
+    print("Você pode tirar sua habilitação!")
+else: 
+        print("Você não pode tirar sua habilitação!")
+
+print("Acabou!")
+    
