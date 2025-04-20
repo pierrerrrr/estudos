a51d67130c3fc47be984407f94106bb21800cea0
Iago Pierre <pierrestorniolo@gmail.com>
2025-04-19 22:34:45 -0300

    commit converstion types and strings

---
+++ classroom/rocketseat/javascript/conversaoType.js
@@ -0,0 +1,18 @@
+// conversão de tipos
+
+// conversão de string para number
+let value = "9";
+
+console.log(typeof value);
+console.log(typeof Number(value));
+
+// conversão de number para string
+let num = 9;
+
+console.log(typeof num);
+console.log(typeof num.toString());
+
+// coerção de tipos - exemplo
+
+console.log( "10"  + 5)
+console.log(typeof  ("10"  + 5))
---
+++ classroom/rocketseat/javascript/string.js
@@ -0,0 +1,6 @@
+// string
+
+userName = "Fernanda";
+console.log(userName);
+
+console.log(typeof userName);
