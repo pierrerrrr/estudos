b251220eb020451185357c347e367d0319c7f202
Iago Pierre <pierrestorniolo@gmail.com>
2025-04-23 23:01:41 -0300

    feats: imported arg imported chalk for the DX imported pkgUp to the configs works right as we expect
    fix: changed to CommonJS to ESM

--- _projects/my-cli/testProject/node_modules/tool/bin/index.js
+++ _projects/my-cli/testProject/node_modules/tool/bin/index.js
@@ -1,2 +1,32 @@
 #!/usr/bin/env node
-console.log("olá mundo");
+import arg from 'arg';
+import chalk from 'chalk'
+import pkgUp from 'pkg-up'
+
+try {
+  const args = arg({
+    "--start": Boolean,
+    "--build": Boolean,
+  });
+
+  if (args["--start"]) {
+    const pkgPath = pkgUp.sync({ cwd: process.cwd() });
+    const pkg = require(pkgPath);
+    if (pkg.tool) {
+      console.log("Found configuration", pkg.tool);
+    } else {
+      console.log(chalk.yellow("Could not find configuration, using default"));
+    }
+    console.log(chalk.bgCyanBright("starting the app..."));
+  }
+} catch (e) {
+  console.log(chalk.yellow(e.message));
+  console.log();
+  usage();
+}
+
+function usage() {
+  console.log(`${chalk.whiteBright("tool[CMD]")}
+    ${chalk.greenBright("--start")}\tStarts the app
+    ${chalk.greenBright("--build")}\tBuilds the app`);
+}
