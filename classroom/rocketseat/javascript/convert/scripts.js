ae589c8259bcccb65cbd7482ac3925c6471e2e2c
Iago Pierre <pierrestorniolo@gmail.com>
2025-05-01 14:58:27 -0300

    up convert app

--- classroom/rocketseat/javascript/convert/scripts.js
+++ classroom/rocketseat/javascript/convert/scripts.js
@@ -1,6 +1,13 @@
+const USD = 5.8;
+const EUR = 6.7;
+const GBP = 7.1;
+
 const form = document.querySelector("form");
 const amount = document.getElementById("amount");
 const currency = document.getElementById("currency");
+const footer = document.querySelector("main footer");
+const description = document.getElementById("description");
+const result = document.getElementById("result");
 
 amount.addEventListener("input", () => {
   const hasCharacterRegex = /\D+/g;
@@ -11,7 +18,36 @@ amount.addEventListener("input", () => {
 form.onsubmit = (e) => {
   e.preventDefault();
 
-  const selectedCurrency = currency.value;
-
-  console.log(selectedCurrency);
+  switch (currency.value) {
+    case "USD":
+      convertCurrency(amount.value, USD, "US$");
+      break;
+    case "EUR":
+      convertCurrency(amount.value, EUR, "€");
+      break;
+    case "GBP":
+      convertCurrency(amount.value, GBP, "£");
+  }
 };
+
+function convertCurrency(amount, price, symbol) {
+  try {
+    description.textContent = `${symbol}1 = ${formatCurrencyBRL(price)}`;
+    const total = amount * price;
+    result.textContent = formatCurrencyBRL(total);
+    footer.classList.add("show-result");
+  } catch (e) {
+    footer.classList.remove("show-result");
+
+    console.log(e);
+    alert("tente de novo");
+  }
+}
+
+function formatCurrencyBRL(value) {
+  // converte string para numero para usar o toLocaleString
+  return Number(value).toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
+}
