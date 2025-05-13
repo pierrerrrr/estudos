ca1f37c45291c4a335d797cf71947e642f5977a0
Iago Pierre <pierrestorniolo@gmail.com>
2025-05-13 12:19:31 -0300

    just added a function to pratice logic

--- classroom/rocketseat/javascript-basico/impar.js
+++ classroom/rocketseat/javascript-basico/impar.js
@@ -7,7 +7,18 @@ resto = num % 2;
 
 console.log(num); // if (SE)
 if (resto === 0) {
-    console.log("Esse número par!");
-} else { // else (SE NÃO)
-    console.log("Esse número é impar!");
-};
+  console.log("Esse número par!");
+} else {
+  // else (SE NÃO)
+  console.log("Esse número é impar!");
+}
+
+
+
+// outro forma de fazer utilizando função
+
+function isEven(num) {
+  return num % 2 === 0 ? "Esse número é par!" : "Esse número é impar";
+}
+
+console.log(isEven(11));
